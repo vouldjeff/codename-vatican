88fe4ee508ce7de8b2288421120a2ed15ce859e8
Dimitar Vouldjeff <vouldjeff@gmail.com>
2011-05-24 14:31:39 -0700

    more...

--- app/helpers/entities_helper.rb
+++ app/helpers/entities_helper.rb
@@ -1,9 +1,10 @@
 module EntitiesHelper
   def visualize_value(field)
   	return if field.nil?
-    if field["key"].nil?
+    if field["type"] != "ref"
       return field["value"]
-    else
+    elsif !field["key"].nil?
+      return field["key"].length < 1
       link_to field["value"], entity_path(:id => field["key"])
     end
   end
